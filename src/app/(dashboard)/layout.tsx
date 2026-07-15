dcdb361a649d8f2e40e66ff51672942213d27361
zheng-5940 <3494735997@qq.com>
2026-07-15 15:27:52 +0800

    Implement client layout in dashboard

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,1 +1,5 @@
-
+"use client";
+import ClientLayout from "./client-layout";
+export default function Layout(props: { children: React.ReactNode }) {
+    return <ClientLayout>{props.children}</ClientLayout>ClientLayout>;
+}</ClientLayout>
