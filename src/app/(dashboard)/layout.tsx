830aabc9f3a0545eecf51585adeb5869220c22b8
zheng-5940 <3494735997@qq.com>
2026-07-15 23:06:06 +0800

    Delete src/app/(dashboard)/layout.tsx

--- src/app/(dashboard)/layout.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import ClientLayout from './client-layout';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-        return <ClientLayout>{children}</ClientLayout>ClientLayout>;
-}</ClientLayout>
